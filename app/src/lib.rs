4aac43637e7376c3114f4f7603926aeb9479322d
Aaron Joslyn <aaronjoslyn@users.noreply.github.com>
2020-09-04 00:27:13 +0100

    Set up wasm lib

---
+++ app/src/lib.rs
@@ -0,0 +1,17 @@
+use wasm_bindgen::prelude::*;
+
+macro_rules! console_log {
+    ($($t:tt)*) => (log(&format_args!($($t)*).to_string()))
+}
+
+#[wasm_bindgen]
+extern "C" {
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(s: &str);
+}
+
+#[wasm_bindgen]
+pub fn main() -> Result<(), JsValue> {
+    console_log!("Loaded.");
+    Ok(())
+}
