a2f66eec9386e4057df7afd28fe83cc089765a38
Aaron Joslyn <aaronjoslyn@users.noreply.github.com>
2020-09-04 20:16:12 +0100

    Connect to websocket server from frontend

--- app/src/lib.rs
+++ app/src/lib.rs
@@ -1,17 +1,39 @@
-use wasm_bindgen::prelude::*;
+use wasm_bindgen::{prelude::*, JsCast};
+use web_sys::{ErrorEvent, MessageEvent, WebSocket};
 
 macro_rules! console_log {
     ($($t:tt)*) => (log(&format_args!($($t)*).to_string()))
+}
+
+macro_rules! console_error {
+    ($($t:tt)*) => (error(&format_args!($($t)*).to_string()))
 }
 
 #[wasm_bindgen]
 extern "C" {
     #[wasm_bindgen(js_namespace = console)]
     fn log(s: &str);
+    fn error(s: &str);
 }
 
 #[wasm_bindgen]
 pub fn main() -> Result<(), JsValue> {
-    console_log!("Loaded.");
+    console_error_panic_hook::set_once();
+    let ws = WebSocket::new("ws://localhost:4000")?;
+    ws.set_binary_type(web_sys::BinaryType::Arraybuffer);
+    let onmessage_callback = Closure::wrap(Box::new(move |e: MessageEvent| {
+        if let Ok(txt) = e.data().dyn_into::<js_sys::JsString>() {
+            console_log!("Received message: {:?}", txt);
+        } else {
+            console_error!("Received unexpected message: {:?}", e.data());
+        }
+    }) as Box<dyn FnMut(MessageEvent)>);
+    ws.set_onmessage(Some(onmessage_callback.as_ref().unchecked_ref()));
+    onmessage_callback.forget();
+    let onerror_callback = Closure::wrap(Box::new(move |e: ErrorEvent| {
+        console_error!("Socket error: {:?}", e);
+    }) as Box<dyn FnMut(ErrorEvent)>);
+    ws.set_onerror(Some(onerror_callback.as_ref().unchecked_ref()));
+    onerror_callback.forget();
     Ok(())
 }
