48226c8f48a8927d0e75127c201c14dc75778180
Aaron Joslyn <aaronjoslyn@users.noreply.github.com>
2020-09-05 12:44:13 +0100

    Create a WebGL context and paint the background

--- app/src/canvas.rs
+++ app/src/canvas.rs
@@ -1,16 +1,19 @@
-pub fn create_canvas() {
+use wasm_bindgen::{JsCast, JsValue};
+use web_sys::{HtmlCanvasElement, WebGlRenderingContext};
+
+pub fn create_canvas() -> Result<(), JsValue> {
     let window = web_sys::window().expect("Failed to find window.");
     let document = window.document().expect("Failed to find document.");
     let body = document.body().expect("Failed to find document body");
-    let canvas = document
-        .create_element("canvas")
-        .expect("Failed to create canvas.");
-    canvas
-        .set_attribute("height", &body.client_height().to_string())
-        .expect("Failed to set height.");
-    canvas
-        .set_attribute("width", &body.client_width().to_string())
-        .expect("Failed to set width.");
-    body.append_child(&canvas)
-        .expect("Failed to append canvas to body.");
+    let canvas: HtmlCanvasElement = document.create_element("canvas")?.dyn_into()?;
+    canvas.set_attribute("height", &body.client_height().to_string())?;
+    canvas.set_attribute("width", &body.client_width().to_string())?;
+    body.append_child(&canvas)?;
+    let gl: WebGlRenderingContext = canvas
+        .get_context("webgl")?
+        .expect("Failed to get WebGL context.")
+        .dyn_into()?;
+    gl.clear_color(0.0, 0.0, 0.0, 1.0);
+    gl.clear(WebGlRenderingContext::COLOR_BUFFER_BIT);
+    Ok(())
 }
